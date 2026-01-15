7d0a60a2d9e1c9a03cc9718272e8637ba965c75e
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-01-15 12:38:25 -0800

    fix build for render php lint errors

--- Blocks/build/Navigation/render.php
+++ Blocks/build/Navigation/render.php
@@ -12,29 +12,18 @@
  * @var WP_Block $block      Block instance.
  */
 
-// Get block attributes with defaults
-$menu_id          = isset( $attributes['menuId'] ) ? (int) $attributes['menuId'] : 0;
-$overlay_menu     = isset( $attributes['overlayMenu'] ) ? $attributes['overlayMenu'] : 'never';
+$menu_id           = isset( $attributes['menuId'] ) ? (int) $attributes['menuId'] : 0;
+$overlay_menu      = isset( $attributes['overlayMenu'] ) ? $attributes['overlayMenu'] : 'never';
 $mobile_breakpoint = isset( $attributes['mobileBreakpoint'] ) ? (int) $attributes['mobileBreakpoint'] : 768;
-$show_in_desktop  = isset( $attributes['showInDesktop'] ) ? (bool) $attributes['showInDesktop'] : true;
-$show_in_mobile   = isset( $attributes['showInMobile'] ) ? (bool) $attributes['showInMobile'] : true;
+$show_in_desktop   = isset( $attributes['showInDesktop'] ) ? (bool) $attributes['showInDesktop'] : true;
+$show_in_mobile    = isset( $attributes['showInMobile'] ) ? (bool) $attributes['showInMobile'] : true;
 
-// Build class names
 $class_names = array(
 	'wp-block-design-system-wordpress-plugin-navigation',
 	'dswp-block-navigation-is-' . esc_attr( $overlay_menu ) . '-overlay',
 );
 
-$wrapper_attributes = get_block_wrapper_attributes(
-	array(
-		'class'                      => implode( ' ', $class_names ),
-		'data-dswp-mobile-breakpoint' => $mobile_breakpoint,
-		'data-show-in-desktop'       => $show_in_desktop ? 'true' : 'false',
-		'data-show-in-mobile'        => $show_in_mobile ? 'true' : 'false',
-	)
-);
-
-// Get navigation menu content from wp_navigation post type
+// Load navigation menu content from wp_navigation post type.
 $navigation_content = '';
 if ( $menu_id > 0 ) {
 	$navigation_post = get_post( $menu_id );
@@ -43,16 +32,15 @@ if ( $menu_id > 0 ) {
 	}
 }
 
-// Parse and render the navigation blocks
 $parsed_blocks = array();
 if ( ! empty( $navigation_content ) ) {
 	$parsed_blocks = parse_blocks( $navigation_content );
 }
 
 ?>
-<nav <?php echo $wrapper_attributes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
-	<button class="dswp-nav-mobile-toggle-icon" aria-label="Toggle menu" aria-expanded="false">
-		<span class="dswp-nav-mobile-menu-icon-text">Menu</span>
+<nav class="<?php echo esc_attr( implode( ' ', $class_names ) ); ?>" data-dswp-mobile-breakpoint="<?php echo esc_attr( (string) $mobile_breakpoint ); ?>" data-show-in-desktop="<?php echo esc_attr( $show_in_desktop ? 'true' : 'false' ); ?>" data-show-in-mobile="<?php echo esc_attr( $show_in_mobile ? 'true' : 'false' ); ?>">
+	<button class="dswp-nav-mobile-toggle-icon" aria-label="<?php echo esc_attr__( 'Toggle menu', 'dswp' ); ?>" aria-expanded="false">
+		<span class="dswp-nav-mobile-menu-icon-text"><?php echo esc_html__( 'Menu', 'dswp' ); ?></span>
 		<svg width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
 			<path class="dswp-nav-mobile-bar dswp-nav-mobile-menu-top-bar" d="M3,6h13" stroke-width="1" stroke="currentColor"></path>
 			<path class="dswp-nav-mobile-bar dswp-nav-mobile-menu-middle-bar" d="M3,12h13" stroke-width="1" stroke="currentColor"></path>
@@ -61,10 +49,10 @@ if ( ! empty( $navigation_content ) ) {
 	</button>
 	<ul class="dswp-block-navigation__container">
 		<?php
-		// Render inner blocks from wp_navigation
 		if ( ! empty( $parsed_blocks ) ) {
 			foreach ( $parsed_blocks as $inner_block ) {
-				echo render_block( $inner_block ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				$block_output = render_block( $inner_block );
+				echo wp_kses_post( $block_output );
 			}
 		}
 		?>
