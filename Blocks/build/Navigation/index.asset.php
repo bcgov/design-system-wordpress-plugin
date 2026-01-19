2b8eb44c192c33a5bdd1b2160c93de69537106a4
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-01-19 15:37:00 -0800

    added mobile view in editor

--- Blocks/build/Navigation/index.asset.php
+++ Blocks/build/Navigation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c1a6743fa04b52a2426e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '99a78bd220c81bd615fc');
