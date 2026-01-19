a835a921d1847156a74eaf81463eb1c293642b46
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-01-19 13:12:11 -0800

    updated block editor for showing mobile menu vs desktop

--- Blocks/build/Navigation/index.asset.php
+++ Blocks/build/Navigation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e8550cb3391026a78f12');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c1a6743fa04b52a2426e');
