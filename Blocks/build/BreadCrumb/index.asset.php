5d1917339b6023f1498793f78bef0d1926016aba
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-03-03 15:23:44 -0800

    removed colors

--- Blocks/build/BreadCrumb/index.asset.php
+++ Blocks/build/BreadCrumb/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd858e223e52158bea5ea');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2c931149cd12eeec13f0');
