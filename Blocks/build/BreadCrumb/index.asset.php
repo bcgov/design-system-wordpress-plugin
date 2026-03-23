7e081f640797c18c54125ebd0f4767a06b75dda7
kshapka-bcgov <kyle.shapka@gov.bc.ca>
2026-03-23 09:19:53 -0700

    Ran linting

--- Blocks/build/BreadCrumb/index.asset.php
+++ Blocks/build/BreadCrumb/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1b6571420b9738f97385');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd067267ee7540496d4c6');
